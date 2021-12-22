b1c8e848f90ce79b80d2a88c9fc680e0d12ca2b2
ryanlopezzzz <ryanlopez@ucsb.edu>
2021-12-22 10:19:35 -0800

    Improved procedure for finding random users

--- instagram_api/collect_user_info_bot.py
+++ instagram_api/collect_user_info_bot.py
@@ -7,15 +7,16 @@ from save_api_data_helpers import add_followers_user_info_to_csv
 import numpy as np
 
 api_username = 'cuteanimalzzzz123'
+print('API username ' + str(api_username))
 api_password = 'instabotOP'
 api = SafeClient(api_username, api_password, retry_failed_call=True, max_api_calls_per_hour=160, 
-                api_call_wait_time_generator = lambda : np.random.uniform(low=40.0, high=60.0))
+                api_call_wait_time_generator = lambda : np.random.uniform(low=4.0, high=8.0))
 user_table_filename = 'saved_info/%s_collected_user_info_table.csv'%api_username
-
 while True:
     random_username = get_random_username(api)
     random_user_info= UserInfo(api, random_username)
-    if random_user_info.follower_count > int(1e3) or random_user_info.private_status is True: #Random user is too popular to collect all followers info 
+    if random_user_info.follower_count > int(1e2) or random_user_info.private_status is True: #Random user is too popular to collect all followers info 
+        print('Found %s  with %d follower_count and private_status '%(random_username, random_user_info.follower_count)) + str(random_user_info.private_status)
         continue
     print('Collecting info on %s\'s %d followers'%(random_username, random_user_info.follower_count))
     add_followers_user_info_to_csv(api, random_username, user_table_filename)
