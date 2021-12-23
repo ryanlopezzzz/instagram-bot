1388c92113f9283d1d1ce4878a56fcb95c776b08
ryanlopezzzz <ryanlopez@ucsb.edu>
2021-12-23 15:52:59 -0800

    Fixed bug for case that comments are disabled on post.

--- instagram_api/bots/interact_with_users_bot.py
+++ instagram_api/bots/interact_with_users_bot.py
@@ -21,9 +21,9 @@ def get_random_comment():
             ['! '],
             ['I '],
             ['will be ', 'plan to be ', 'am going to be '],
-            ['posting a nice picture and inspirational quote everyday like this: '],
+            ['posting a nice picture and inspirational quote everyday like this: \"'],
             [random.choice(requests.get('https://type.fit/api/quotes').json())['text']],
-            [' I would love if you checked my page out!']
+            ['\" I would love if you checked my page out!']
         ]
         random_comment = ''
         for component in comment_components:
@@ -38,17 +38,17 @@ api = SafeClient(api_username, api_password, retry_failed_call=True, max_api_cal
                 api_call_wait_time_generator = lambda : np.random.uniform(low=25.0, high=50.0))
 print('Logged in')
 
-#Load table of user info
-table_username = 'cuteanimalzzzz123'
-user_table_filename = os.path.join(instagram_api_folder,'saved_info/%s_collected_user_info_table.csv'%table_username)
-if os.path.isfile(user_table_filename):
-    loaded_user_table = pd.read_csv(user_table_filename)
-    loaded_user_table.set_index('user_id', inplace=True)
-else:
-    loaded_user_table = None
-print('Loaded user table')
-
 while True:
+    #Load table of user info
+    table_username = 'cuteanimalzzzz123'
+    user_table_filename = os.path.join(instagram_api_folder,'saved_info/%s_collected_user_info_table.csv'%table_username)
+    if os.path.isfile(user_table_filename):
+        loaded_user_table = pd.read_csv(user_table_filename)
+        loaded_user_table.set_index('user_id', inplace=True)
+    else:
+        loaded_user_table = None
+    print('Loaded user table')
+
     #Load interaction table for api user
     interaction_columns = ['media_id', 'user_id', 'media_liked', 'comment_left', 'api_request_time']
     interaction_table_filename = os.path.join(instagram_api_folder,'saved_info/%s_interacted_with_table.csv'%api_username)
@@ -65,19 +65,27 @@ while True:
     ranked_table = ranked_table[~users_interacted_with]
     max_interaction_user_id = ranked_table['interaction_score'].idxmax()
     max_interaction_username = ranked_table.loc[max_interaction_user_id]['username']
-    print('Got username to interact with')
+    print('Got username to interact with %s'%max_interaction_username)
 
     #Like users first post and make random comment
-    first_media_id = api.user_feed(max_interaction_user_id)['items'][0]['id']
-    print(first_media_id)
+    first_media = api.user_feed(max_interaction_user_id)['items'][0]
+    first_media_id = first_media['id']
 
-    random_comment = get_random_comment()
     iwua = InteractWithUsersActions(api)
     api_request_time = time.time()
     iwua.like_post(first_media_id)
-    iwua.post_comment(first_media_id, random_comment)
-    print('Liked and commented on a post')
+    print('Liked Post')
 
+    commenting_disabled_key = 'commenting_disabled_for_viewer'
+    if commenting_disabled_key not in first_media.keys(): #comments are enabled
+        random_comment = get_random_comment()
+        iwua.post_comment(first_media_id, random_comment)
+        print('Commented on a post')
+    elif first_media[commenting_disabled_key] == True: #comments disabled
+        print('Commenting on post is disabled')
+        random_comment = ''
+    else:
+        print('This should not occur, I think.')
     new_row = pd.DataFrame([[first_media_id, max_interaction_user_id, True, random_comment, api_request_time]], columns=interaction_columns)
     new_row.set_index('media_id', inplace=True)
     new_interaction_table = pd.concat([loaded_interaction_table, new_row])
