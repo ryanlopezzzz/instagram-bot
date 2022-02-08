3e867fdf1144004baa4798447e99cd003107c977
ryanlopezzzz <ryanlopez@ucsb.edu>
2022-02-08 13:53:35 -0800

    Minor edits and cleaning up code

--- instagram_api/bots/collect_media_info_bot.py
+++ instagram_api/bots/collect_media_info_bot.py
@@ -1,6 +1,8 @@
 """
 Collects media (post) info from users we've collected user info on.
 """
+from instagram_private_api import ClientError
+import time
 import numpy as np
 import pandas as pd
 import os
@@ -11,6 +13,9 @@ from utils.safe_api import (SafeClientExtended, RequestingBadInfoException)
 from utils.save_api_data_helpers import (add_user_feed_to_csv, user_id_in_table)
 
 def should_collect_media_info_on_user(user_id, user_table, media_table):
+    """
+    Should only collect media data on novel, public accounts with posts
+    """
     if user_id_in_table(user_id, media_table) == True:
         return False
     elif user_table.loc[user_id].private_status == True:
@@ -22,9 +27,8 @@ def should_collect_media_info_on_user(user_id, user_table, media_table):
 
 #Log In
 api_username = 'cuteanimalzzzz123'
-api_password = 'instabotOP'
-api = SafeClientExtended(api_username, api_password, retry_failed_call=True, max_api_calls_per_hour=160, 
-                api_call_wait_time_generator = lambda : np.random.uniform(low=8.0, high=16.0))
+api = SafeClientExtended(api_username, retry_failed_call=True, max_api_calls_per_hour=160, 
+                api_call_wait_time_generator = lambda : np.random.uniform(low=10.0, high=20.0))
 print('Logged in to user %s'%api_username)
 
 #Load user info table
@@ -41,12 +45,27 @@ if os.path.isfile(media_table_filename):
 else:
     loaded_media_table = None
 
+#previous users which have given client errors.
+bad_user_ids = [51182897960, 13053481207, 1295208947, 4350769776, 44299115548, 44753617779, 
+                32788714015, 12364239772, 50750437882, 6230984983, 5872393888, 1811694664, 13096914, 
+                1077448382, 49823679205, 50820893565, 50740850513, 46272456547, 48843118405, 27396305200]
+
 #Run data collecting bot
+client_errors = 0
 for user_id in loaded_user_table.index.values:
     if should_collect_media_info_on_user(user_id, loaded_user_table, loaded_media_table):
+        if user_id in bad_user_ids:
+            continue
         print(f'Collecting media data on user id {user_id}')
         try:
             _ = add_user_feed_to_csv(api, user_id, media_table_filename, num_results_stop_api_at = 100)
         except RequestingBadInfoException as e:
             print('Exception: %s, Message: %s'%(type(e).__name__,e))
+        except ClientError as e:
+            print('Exception: %s, Message: %s'%(type(e).__name__,e))
+            client_errors+=1
+            if client_errors == 3:
+                print('Too many client errors')
+                break
+            time.sleep(60)
 print('Completed data collection for current user table')
